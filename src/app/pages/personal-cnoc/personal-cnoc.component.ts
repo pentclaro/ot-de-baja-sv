71bd8bcc6b1f0ec301f3cee127d6722e264ff207
DaverSoGT <jodaniheso@gmail.com>
2022-05-26 11:59:24 -0600

    Creación de componente para el nuevo reporte

---
+++ src/app/pages/personal-cnoc/personal-cnoc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-cnoc',
+  templateUrl: './personal-cnoc.component.html',
+  styleUrls: ['./personal-cnoc.component.scss']
+})
+export class PersonalCnocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
