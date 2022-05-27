c6c6d5655a6d00b0d6f09cfedda37ae9214ce7c2
DaverSoGT <jodaniheso@gmail.com>
2022-05-27 10:18:54 -0600

    Creación de tabla w2ui y ocultación de menú de navegación

---
+++ src/app/services/personal-cnoc/personal-cnoc.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeaderService } from '../header.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonalCNOCService {
+
+  constructor(
+    private http: HttpClient,
+    private queryService: HeaderService
+  ) { }
+
+  obtenerPersonalCNOC() {
+    return this.http.get(`${environment.apiURL}/personalCNOC/obtenerPersonalCNOC`);
+  }
+}
