841f3c9da2b528ee2c2c69fb85395eae96928e38
DaverSoGT <jodaniheso@gmail.com>
2022-09-21 11:54:56 -0600

    Inicializacion de reporte tickets WO

---
+++ src/app/pages/tickets-wo/tickets-wo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-wo',
+  templateUrl: './tickets-wo.component.html',
+  styleUrls: ['./tickets-wo.component.scss']
+})
+export class TicketsWOComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
